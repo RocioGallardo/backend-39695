afd00f2a5199f479bf3f7fc884dd208570ca2b02
Rocio <hola.rogallardo@gmail.com>
2023-02-09 15:21:30 -0300

    clase 4

---
+++ Clases/clase4/script.js
@@ -0,0 +1,73 @@
+// Math.sqrt(25)
+
+
+//const fs = require("fs")
+
+const {readFileSync, // lee un archivo
+        writeFileSync, // [sobre]escribe un archivo
+        appendFileSync, // agrega contenido a un archivo
+        rmSync // (remove) borra un archivo
+    } = require("fs")
+
+//console.log(readFileSync)
+
+//READING
+
+try {
+    const contenidoTimers = readFileSync("./timers.js", "utf-8")
+    console.log(contenidoTimers)
+} catch (error) {
+    console.log(error.message)
+}
+
+
+//WRITING
+
+try {
+    writeFileSync("./nuevo.txt", "hola")
+} catch (error) {
+    console.log(error.message)
+}
+
+
+// APPEND - agregar al final
+
+try {
+    appendFileSync("./nuevo.txt", ` 
+    este texto está adosado`)
+} catch (error) {
+    console.log(error.message)
+}
+
+
+// REMOVE - remueve un archivo o directorio
+
+try {
+    rmSync("./nuevo.txt")
+} catch (error) {
+    console.log(error.message)
+}
+
+// rename, copy, readdirSync(leer contenido de un directorio)
+
+
+// PROMESAS
+
+const {
+    promises: {
+        readFile, // lee un archivo
+        writeFile, // [sobre]escribe un archivo
+        appendFile, // agrega contenido a un archivo
+        rm // (remove) borra un archivo
+    }
+} = require("fs")
+
+
+async function main(){
+    try {
+        const contenidoTimers = await readFile("./timers.js", "utf-8")
+        console.log(contenidoTimers)
+    } catch (error) {
+        console.log(error.message)
+    }
+}
---
+++ Clases/clase4/timers.js
@@ -0,0 +1,28 @@
+// function callback(){
+//     console.log("hola mundo")
+// }
+
+// const tiempoDeEspera = 30_000
+
+
+//_______________________________
+
+
+// const timer1 = setTimeout(callback, tiempoDeEspera)
+
+// setTimeout(() => {
+//     clearTimeout(timer1)
+//     console.log("cancelado el primero")
+// }, 3000)
+
+
+//_______________________________
+
+// const timer2 = setInterval(callback, tiempoDeEspera)
+
+// setTimeout(() => {
+//     clearInterval(timer2)
+//     console.log("cancelado el segundo")
+// }, 3000)
+
+
