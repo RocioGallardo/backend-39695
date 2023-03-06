6a22f20b90633100cef107d23bbd3945cd7bafaf
Rocio <hola.rogallardo@gmail.com>
2023-03-06 16:02:26 -0300

    1 Nivelacion

---
+++ Clases/1_Nivelacion/main.js
@@ -0,0 +1,54 @@
+// Objetivo principal : aprender a hacer servidores
+
+/*
+
+FrontEnd comprende toda la parte visual y de interaccion directa con el usuario
+    - Imágenes
+    - Colores
+    - Botones
+    - Interacciones
+
+Backend comprende toda la parte lógica y de manejo de informacion
+    - Almacenamiendo de informacion
+    - Calculos complejos
+    - Servidores donde viven las páginas
+    - Manejo de informacion en general
+
+_______________________________________
+
+STACK MERN
+
+Ciertas tecnologgías que en conjunto nos brindarán la posibilidad de desarrollar sistemas completos, en este caso
+    M - MongoDB
+    E - ExpressJS
+    R - ReactJS
+    N - Nodejs
+
+_______________________________________
+
+Maneras de probar JS
+
+    - Web ( navegador )
+    - Node js 
+
+Tipos de datos en Javascript
+
+--- Lenguaje tipado dinámico ---
+cada dato carga su propio tipo
+
+Tipos primitivos 
+    - string
+    - number
+    - boolean
+    - null
+    - undefined
+
+Tipos objeto
+    - Predefinidos de JS (Date, Error) 
+    - Definidos por programador/usuario (Funciones simples, clases)
+    - Arrays
+    - Diccionario
+
+
+
+*/
