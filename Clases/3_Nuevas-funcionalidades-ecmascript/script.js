6db73e911c029c9aec5cb8c421ebd06c5905194e
Rocio <hola.rogallardo@gmail.com>
2023-03-09 19:45:36 -0300

    3_ nuevas funcionalidades ECMA

---
+++ Clases/3_Nuevas-funcionalidades-ecmascript/script.js
@@ -0,0 +1,32 @@
+/*
+ES7
+    - exponencial **
+    - array includes
+
+ES8
+    - async await */
+fetch("https://jsonplaceholder.typicode.com/users/1/posts")//envío petición, nos devuelve una promesa
+.then(res => res.json())
+/*
+    - object.entries
+    - object.values
+    - object.keys
+
+ES9
+    - .finally()
+    - operadores rest
+    - spread
+
+ES10
+    - string.trim()
+    - array.flat()
+    - Dinamyc import
+
+ES11
+    - operador nullish
+    - variables privadas
+
+
+
+
+*/
