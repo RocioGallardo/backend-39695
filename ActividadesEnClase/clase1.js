c565e212e55f335cb54e4b64a9b398a871a8981c
Rocio <hola.rogallardo@gmail.com>
2023-03-22 16:25:29 -0300

    actividad en clase

---
+++ ActividadesEnClase/clase1.js
@@ -0,0 +1,23 @@
+// Definir variables que almacenen datos (nombre, edad, precio, 
+// nombres de series y películas), mostrar esos valores por consola, 
+// incrementar la edad en 1, una serie a la lista y volver a mostrarlas. 
+// Compartir la definición en el Visual Studio Code.
+
+let nombre = "rocio"
+let edad = 27
+let precio = 100
+let nombresDeSeries = ["The last of us", "Grey's anatomy"]  
+let peliculas = ["El secreto de sus ojos", "Enredados"]
+
+
+console.log(nombre)
+console.log(edad)
+console.log(precio)
+console.log(nombresDeSeries)
+console.log(peliculas)
+
+edad++
+nombresDeSeries.push("dark")
+
+console.log(edad)
+console.log(nombresDeSeries)
