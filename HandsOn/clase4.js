6ff9a4cd689eb02566c62203c54896bd18c1dbc2
Rocio <hola.rogallardo@gmail.com>
2023-03-23 15:22:18 -0300

    Hands on

--- HandsOn/clase4.js
+++ HandsOn/clase4.js
@@ -15,3 +15,84 @@
 // Definir la misma función división utilizada en esta clase.
 // Definir una función asíncrona “cálculos”, y realizar pruebas utilizando async/await y try/catch
 
+
+// Función suma
+function suma(a, b) {
+    return new Promise((resolve, reject) => {
+        if (a === 0 || b === 0) {
+            reject("Operación innecesaria");
+        } else {
+            const result = a + b;
+            if (result < 0) {
+                reject("La calculadora sólo debe devolver valores positivos");
+            } else {
+                resolve(result);
+            }
+        }
+    });
+}
+
+// Función resta
+function resta(a, b) {
+    return new Promise((resolve, reject) => {
+        if (a === 0 || b === 0) {
+            reject("Operación inválida");
+        } else {
+            const result = a - b;
+            if (result < 0) {
+                reject("La calculadora sólo puede devolver valores positivos");
+            } else {
+                resolve(result);
+            }
+        }
+    });
+}
+
+// Función multiplicación
+function multiplicacion(a, b) {
+    return new Promise((resolve, reject) => {
+        if (a < 0 || b < 0) {
+            reject("La calculadora sólo puede devolver valores positivos");
+        } else {
+            const result = a * b;
+            resolve(result);
+        }
+    });
+}
+
+// Función división
+function division(a, b) {
+    return new Promise((resolve, reject) => {
+        if (b === 0) {
+            reject("No se puede dividir por cero");
+        } else {
+            const result = a / b;
+            resolve(result);
+        }
+    });
+}
+
+// Función asíncrona "cálculos"
+async function calculos() {
+    try {
+        const resultadoSuma = await suma(2, 3);
+        console.log("El resultado de la suma es:", resultadoSuma);
+
+        const resultadoResta = await resta(10, 5);
+        console.log("El resultado de la resta es:", resultadoResta);
+
+        const resultadoMultiplicacion = await multiplicacion(4, 6);
+        console.log("El resultado de la multiplicación es:", resultadoMultiplicacion);
+
+        const resultadoDivision = await division(8, 2);
+        console.log("El resultado de la división es:", resultadoDivision);
+
+        const resultadoDivisionCero = await division(5, 0);
+        console.log("El resultado de la división es:", resultadoDivisionCero);
+    } catch (error) {
+        console.error("Ocurrió un error:", error);
+    }
+}
+
+// Llamada a la función asíncrona "cálculos"
+calculos();
