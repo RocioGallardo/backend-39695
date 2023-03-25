ec57c8e2d7595b2a67f923320ee00e5497040c84
Rocio <hola.rogallardo@gmail.com>
2023-03-25 16:43:19 -0300

    clase8 express avanzado

---
+++ Clases/8_Express-avanzado/src/main.js
@@ -0,0 +1,25 @@
+/* 
+CÓDIGOS DE ESTADO HTTP
+
+1xx: Status informativo,
+2xx: Status ok,
+3xx: Status de redirección,
+4xx: Status de error de cliente,
+5xx: Status de error en servidor
+
+Recuerda que tú eres el desarrollador del servidor, entonces es tu responsabilidad reconocer cuándo colocar cada código de status. Si no configuramos nuestro servidor para devolver múltiples status, entonces será mucho más difícil rastrear los problemas. 
+
+
+API (Application Programming Interface)
+
+Métodos de petición
+GET: Obtener un recurso
+POST: Crear o añadir un recurso
+PUT: Modificar un recurso
+DELETE: Eliminar un recurso
+
+
+
+*/
+
+
